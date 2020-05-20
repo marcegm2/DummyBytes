7273a374c066e732d27801c84d77db8f35f188b8
mcicerigm2 <marcelo@gm2dev.com>
2020-05-20 12:42:23 -0300

    add widget running on staging

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
-echo "<h1>Feliz cumple JP</h1>";
+echo "<h1>Widget running on staging</h1>";
+
+?>
+
+<div id="yappa-comments-frame"></div>
+
+<script type="text/javascript" src="https://stage-comments.yappaapp.com/embed/yappa-comments.js"></script>
+
