2a2add6dbbcd94ec7f6b06a5c4e07063acd594be
mcicerigm2 <marcelo@gm2dev.com>
2020-05-27 16:38:56 -0300

    add qa env

---
+++ qa-yappa.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h1>Widget running on staging</h1>";
+
+?>
+
+<div id="yappa-comments-frame"></div>
+
+<script type="text/javascript" src="https://qa-comments.yappaapp.com/embed/yappa-comments.js"></script>
