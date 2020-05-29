4e6ff50328933ca10322e9d2c389f78a827005d5
mcicerigm2 <marcelo@gm2dev.com>
2020-05-28 22:23:41 -0300

    new widget

---
+++ qa-yappa-2.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h1>Widget running on QA</h1>";
+
+?>
+
+<h3>Contame todo please baby!</h3>
+<div id="yappa-comments-frame"></div>
+
+<script type="text/javascript" src="https://qa-comments.yappaapp.com/embed/yappa-comments.js"></script>
