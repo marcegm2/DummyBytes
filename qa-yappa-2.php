4b0d471f7a16ca56fb6a754abb5da2fe870d454b
mcicerigm2 <marcelo@gm2dev.com>
2020-05-28 22:27:46 -0300

    update title

--- qa-yappa-2.php
+++ qa-yappa-2.php
@@ -4,7 +4,7 @@ echo "<h1>Widget running on QA</h1>";
 
 ?>
 
-<h3>Contame todo please baby!</h3>
+<h3>Crees que chelo tiene que volver a la musica?</h3>
 <div id="yappa-comments-frame"></div>
 
 <script type="text/javascript" src="https://qa-comments.yappaapp.com/embed/yappa-comments.js"></script>
