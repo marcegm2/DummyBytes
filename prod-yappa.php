4684c9d9d288c1bf34a9c58f50f6e785c8a22eb6
mcicerigm2 <marcelo@gm2dev.com>
2020-05-28 23:48:31 -0300

    widget for prod

--- prod-yappa.php
+++ prod-yappa.php
@@ -6,4 +6,4 @@ echo "<h1>Widget running on new PROD</h1>";
 
 <div id="yappa-comments-frame"></div>
 
-<script type="text/javascript" src="https://qa-comments.yappaapp.com/embed/yappa-comments.js"></script>
+<script type="text/javascript" src="https://prod-comments.yappaapp.com/embed/yappa-comments.js"></script>
